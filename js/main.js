e741c23552ad5326647565436c0484a263ede111
KitsuneVog <86886280+KitsuneVog@users.noreply.github.com>
2024-07-30 08:13:39 +0300

    /

--- js/main.js
+++ js/main.js
@@ -9,33 +9,42 @@ function myLoop() {
 	setTimeout(function() {
 		let diagonal_0 = document.getElementById('diagonal_0').value
 		let diagonal_1 = document.getElementById('diagonal_1').value
+		let attitude = document.getElementById('attitude').value
 
 		document.getElementById('display-1_text').innerHTML = diagonal_0
 		document.getElementById('display-2_text').innerHTML = diagonal_1
+
+		if (i) {
+			window.scrollTo({ top: 1000, behavior: 'smooth' });
+
+			let l = attitude.split(" ")
+			a = l[0]
+			b = l[1]
+			
+			let x = (diagonal_0 ** 2 / (a * 2 + b * 2)) ** 0.5
+
+			let width = a * x
+			let height = b * x
+
+			console.log(width, height);
+
+			x = (diagonal_1 ** 2 / (a * 2 + b * 2)) ** 0.5
+
+			width = a * x
+			height = b * x
+
+			console.log(width, height);
+
+			i = false
+		}
 
 		let ratio = diagonal_0 / diagonal_1 - 1
 
 		document.body.style.cssText += `--size: ${ratio}`
 		document.body.style.cssText += `--size-0: ${screenWidth}px`
 
-		let n1 = screenWidth - screenWidth * ratio.toFixed(3)
-		let n2 = screenHeight - screenHeight * ratio.toFixed(3)
-		let resolution = n1 + " x " + n2
-
-		if (i) {
-			window.scrollTo({ top: 1000, behavior: 'smooth' });
-			console.log(resolution);
-			i = false
-		}
-
-
 		myLoop();
 	}, 100)
 }
 
 myLoop();
-
-
-
-// a^2 + b^2 == c^2
-
